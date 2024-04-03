1d06aed61a35a80b1801a5fc1cf0697657c5dc46
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-04-03 16:36:44 -0300

    adding MessageUtil class

---
+++ src/main/java/com/matt/libraryapi/utils/MessageUtil.java
@@ -0,0 +1,11 @@
+package com.matt.libraryapi.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageUtil {
+
+  private final String message;
+}
