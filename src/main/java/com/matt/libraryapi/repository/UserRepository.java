f989ce52be1c4d3fc1a555279770c9c8f0114776
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-04-19 18:25:46 -0300

    adding findByEmail method to UserRepository interface

--- src/main/java/com/matt/libraryapi/repository/UserRepository.java
+++ src/main/java/com/matt/libraryapi/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.matt.libraryapi.repository;
 import com.matt.libraryapi.domain.entity.User;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
   User findByCpf(String cpf);
+
+  UserDetails findByEmail(String email);
 }
