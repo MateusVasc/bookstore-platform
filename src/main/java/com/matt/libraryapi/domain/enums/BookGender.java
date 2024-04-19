3fe04982142d1d8ab72a54178f3c841c32834625
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-04-19 18:27:25 -0300

    updating BookGender enum

--- src/main/java/com/matt/libraryapi/domain/enums/BookGender.java
+++ src/main/java/com/matt/libraryapi/domain/enums/BookGender.java
@@ -1,25 +1,35 @@
 package com.matt.libraryapi.domain.enums;
 
 public enum BookGender {
-  ROMANCE,
-  SCIENCE_FICTION,
-  FANTASY,
-  MYSTERY,
-  SUSPENSE,
-  THRILLER,
-  HORROR,
-  ADVENTURE,
-  DETECTIVE,
-  DRAMA,
-  PHILOSOPHY,
-  BIOGRAPHY,
-  AUTOBIOGRAPHY,
-  HISTORY,
-  POLITICS,
-  SCIENCE,
-  TECHNOLOGY,
-  SELF_HELP,
-  GEOGRAPHY,
-  MATHEMATICS,
-  PSYCHOLOGY
+  ROMANCE("romance"),
+  SCIENCE_FICTION("science fiction"),
+  FANTASY("fantasy"),
+  MYSTERY("mystery"),
+  SUSPENSE("suspense"),
+  THRILLER("thriller"),
+  HORROR("horror"),
+  ADVENTURE("adventure"),
+  DETECTIVE("detective"),
+  DRAMA("drama"),
+  PHILOSOPHY("philosophy"),
+  BIOGRAPHY("biografy"),
+  AUTOBIOGRAPHY("autobiografy"),
+  HISTORY("history"),
+  POLITICS("politics"),
+  SCIENCE("science"),
+  TECHNOLOGY("technology"),
+  SELF_HELP("self help"),
+  GEOGRAPHY("geography"),
+  MATHEMATICS("mathematics"),
+  PSYCHOLOGY("psychology");
+
+  private String gender;
+
+  BookGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getGender() {
+    return this.gender;
+  }
 }
