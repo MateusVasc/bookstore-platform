4761aad5ff588af51effcde1e1ad90ca31e2ed6b
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-04-19 18:23:21 -0300

    updating project
    renaming CreateUserRequestDTO and CreateUserResponseDTO

--- src/main/java/com/matt/libraryapi/domain/response/RegisterResponseDTO.java
+++ src/main/java/com/matt/libraryapi/domain/response/RegisterResponseDTO.java
@@ -1,9 +1,10 @@
 package com.matt.libraryapi.domain.response;
 
+import com.matt.libraryapi.domain.enums.Role;
 import com.matt.libraryapi.domain.enums.UserGender;
 
 public record CreateUserResponseDTO(String name, String lastname, UserGender gender, int age,
                                     String email, String cpf, boolean isRenting,
-                                    boolean isMissingReturnDate, boolean isBlocked) {
+                                    boolean isMissingReturnDate, boolean isBlocked, Role role) {
 
 }
