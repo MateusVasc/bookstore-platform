44d23aacdc7c133c93cdee317d11098fae66bd3b
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-04-19 18:30:05 -0300

    updating ResgisterResponseDTO record

--- src/main/java/com/matt/libraryapi/domain/response/RegisterResponseDTO.java
+++ src/main/java/com/matt/libraryapi/domain/response/RegisterResponseDTO.java
@@ -3,8 +3,8 @@ package com.matt.libraryapi.domain.response;
 import com.matt.libraryapi.domain.enums.Role;
 import com.matt.libraryapi.domain.enums.UserGender;
 
-public record CreateUserResponseDTO(String name, String lastname, UserGender gender, int age,
-                                    String email, String cpf, boolean isRenting,
-                                    boolean isMissingReturnDate, boolean isBlocked, Role role) {
+public record RegisterResponseDTO(String name, String lastname, UserGender gender, String email,
+                                  String cpf, boolean isRenting, boolean isMissingReturnDate,
+                                  boolean isBlocked, Role role) {
 
 }
