1c4ec9ef8d162919d68f33191fe5a9be9bb80a6b
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-04-22 15:36:35 -0300

    adding TokenService class

--- src/main/java/com/matt/libraryapi/infra/security/TokenService.java
+++ src/main/java/com/matt/libraryapi/infra/security/TokenService.java
@@ -1,2 +1,52 @@
-package com.matt.libraryapi.infra.security;public class TokenService {
+package com.matt.libraryapi.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.matt.libraryapi.domain.entity.User;
+import com.matt.libraryapi.utils.LibraryException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+  private static final String SECRET_KEY = System.getenv("SECRET_KEY");
+
+  public String generateToken(User user) throws LibraryException {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+
+      return JWT.create()
+          .withIssuer("auth-api")
+          .withSubject(user.getEmail())
+          .withExpiresAt(generateExpirationDate())
+          .sign(algorithm);
+
+    } catch (JWTCreationException exception) {
+      throw new LibraryException("Error while creating token");
+    }
+  }
+
+  public String validateToken(String token) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+
+      return JWT.require(algorithm)
+          .withIssuer("auth-api")
+          .build()
+          .verify(token)
+          .getSubject();
+
+    } catch (JWTVerificationException exception) {
+      return "";
+    }
+  }
+
+  private Instant generateExpirationDate() {
+    return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+  }
 }
