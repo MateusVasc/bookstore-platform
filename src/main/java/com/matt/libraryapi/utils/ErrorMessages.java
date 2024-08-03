be58bbb1a1a7edf6e3a769cbf670f7758e9431c0
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-08-02 22:21:22 -0300

    feat: Adds unauthorized error message

--- src/main/java/com/matt/libraryapi/utils/ErrorMessages.java
+++ src/main/java/com/matt/libraryapi/utils/ErrorMessages.java
@@ -6,6 +6,7 @@ public class ErrorMessages {
   public static final String EMAIL_NOT_REGISTERED = "Email not registered";
   public static final String EMAIL_USED = "Email already used";
   public static final String INVALID_DATA = "Invalid data";
+  public static final String UNAUTHORIZED_USER = "Only an admin user can make this action";
 
   private ErrorMessages() {
     throw new UnsupportedOperationException(
