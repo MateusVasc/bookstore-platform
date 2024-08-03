935a5a2f6b0ad8bc3d573813fe8e6d52a712f391
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-08-02 22:23:52 -0300

    feat: Adds endpoint to service

--- src/main/java/com/matt/libraryapi/controllers/AdminController.java
+++ src/main/java/com/matt/libraryapi/controllers/AdminController.java
@@ -2,9 +2,11 @@ package com.matt.libraryapi.controllers;
 
 import com.matt.libraryapi.domain.requests.SaveBookRequest;
 import com.matt.libraryapi.services.AdminService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +19,9 @@ public class AdminController {
 
   private final AdminService adminService;
 
-  @PostMapping("/new-book")
-  public ResponseEntity<Void> saveBook(@RequestBody SaveBookRequest request) {
-    this.adminService.saveBook(request);
+  @PostMapping("/{adminId}/new-book")
+  public ResponseEntity<Void> saveBook(@PathVariable UUID adminId, @RequestBody SaveBookRequest request) {
+    this.adminService.saveBook(adminId, request);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
